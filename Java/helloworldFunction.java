c362ab4f3b14d027de775e9af753614bd69a06de
piumi96 <piumidp96@gmail.com>
2018-10-05 18:02:39 +0530

    simple java function for hello world

---
+++ Java/helloworldFunction.java
@@ -0,0 +1,10 @@
+public class helloworldFunction{
+	public static void main(String args[]){
+		sayHello();
+	}
+
+    //simple function to print hello world
+	private static void sayHello(){
+		System.out.println("Hello world");
+	}
+}
