a4f27b1ca6feb3970f1e76e90ebddf77c795e63b
Valentin <38182450+itsVale@users.noreply.github.com>
2018-10-07 18:07:59 +0200

    Adding another Hello World! script. (#67)

---
+++ Python/exotic_hello_world.py
@@ -0,0 +1,20 @@
+# Oh Jesus, this took forever.
+
+getattr(
+    __import__(([].__class__.__name__ == [()].__class__.__name__).__class__.__name__[1] + [""].__class__.__name__[::-1][1]),
+    ().__class__.__eq__.__class__.__name__[:2] + ([]).__iter__().__class__.__name__[5:8]
+)(
+    int.from_bytes(b'\x00\x01', byteorder='big'),
+    bytes(
+        (lambda _, __: _(_, __))(
+            lambda _, __: chr(__ % 256) + _(_, __ // 256) if __ else str.__new__(str),
+            (
+                lambda ____: sum(
+                    (lambda ___: ____[___] * 256 ** ___)(___) for ___ in range(len(____))))
+            ((lambda __: [ord(_) for _ in __])(str.__new__(str).join([
+                chr(_) for _ in [100-28, 100+1, 100+8, 100+8, 100+11, 100+60, 100-13, 100+11, 100+14, 100+8, 100, 100-67]
+            ])))
+        ).encode(
+            ().__class__.__name__[:2][::-1] + filter.__doc__[0] + int.from_bytes(b'\x00\x08', byteorder='big').__str__()
+        ))
+)
