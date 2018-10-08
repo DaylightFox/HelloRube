b34971cb80864f8c522d4640f4155c45044aea49
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-10-09 01:00:47 +0530

    Added Author Bhautik in File

--- Javascript/arrayhelloworld.js
+++ Javascript/arrayhelloworld.js
@@ -1,9 +1,12 @@
-let string = "Hello World"
-let words = string.split(" ")
-function print (){
-	words.map(function(word){
-		console.log(word)
-	})
+/**
+ * @author Bhautik Bharadava
+ */
+let string = "Hello World";
+let words = string.split(" ");
+function print() {
+  words.map(function(word) {
+    console.log(word);
+  });
 }
 
-print()
+print();
