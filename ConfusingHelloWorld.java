a1997aff533350a219f2bce3a8e1571a81170f7a
Shrey Aeron <s-aeronsh@bsd405.org>
2018-10-08 05:30:17 -0700

    Create ConfusingHelloWorld.java (#71)

---
+++ ConfusingHelloWorld.java
@@ -0,0 +1,46 @@
+public class ConfusingHelloWorld{
+  public static void main(String[] args){
+    h();
+    e();
+    l();
+    l();
+    o();
+    comma();
+    space();
+    w();
+    o();
+    r();
+    l();
+    d();
+  }
+  public static void h(){
+    System.out.print("h");
+  }
+  public static void e(){
+    System.out.print("e");
+  }
+  public static void l(){
+    System.out.print("l");
+  }
+  public static void o(){
+    System.out.print("o");
+  }
+  public static void comma(){
+    System.out.print(",");
+  }
+  public static void space(){
+    System.out.print(" ");
+  }
+  public static void w(){
+    System.out.print("w");
+  }
+  public static void r(){
+    System.out.print("r");
+  }
+  public static void l(){
+    System.out.print("l");
+  }
+  public static void d(){
+    System.out.print("d");
+  }
+}
