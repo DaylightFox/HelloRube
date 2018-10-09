854e9f6b915fb9a90726fb530e1af205826f0c78
BallisticZer0 <37849667+BallisticZer0@users.noreply.github.com>
2018-10-09 11:01:14 -0600

    Add a random way (#80)

---
+++ Python/Hello_Random.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+HACKTOBERFEST = 100
+
+def hacktoberfest():
+    i = random.randint(1,5)
+    print("The hacktoberfest number is...", i)
+    return i
+
+
+def HACKtoberFEST():
+    print("Hello, World!")
+
+
+
+while HACKTOBERFEST == 100:
+    time.sleep(1)
+
+
+
+    if hacktoberfest() == 3:
+        HACKtoberFEST()
+        break
