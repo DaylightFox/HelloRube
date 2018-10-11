bbfd0c8b9492d5aec10e4a92e7c474bff969af3d
ama3489 <32362357+ama3489@users.noreply.github.com>
2018-10-11 12:37:24 -0400

    Added helloworldUser

---
+++ C++/helloworldUser.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string name;
+
+    cout <<"Enter your name." << endl;
+    cin >> name;
+
+
+    cout << name <<": Hello World!" << endl;
+    cout << "World: Hello " << name <<"!" << endl;
+
+    return 0;
+}
