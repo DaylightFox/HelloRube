19f52c362ddbde1dbbf20d79e92e9d631c2cca93
Tim Preble <timpreble@tims-mbp.attlocal.net>
2018-10-16 21:08:35 -0400

    Hello Binary

--- Python/helloBin.py
+++ Python/helloBin.py
@@ -3,7 +3,6 @@ str2 = "0b0111011101101111011100100110110001100100"
 
 def convertBits(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits, 2)
-    #return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
 
 print(convertBits(str1) + ', ' + convertBits(str2) + "!")
