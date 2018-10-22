23117260c220b137e38380ea7acbb9e1608dec1d
teaganneethling <39373544+teaganneethling@users.noreply.github.com>
2018-10-22 16:29:59 +0200

    Add simple not simple JS example (#95)
    Adds a JS Hello World example that's really just silly.

---
+++ Javascript/simple-not-simple.js
@@ -0,0 +1,13 @@
+function say() {
+  return 'Hello';
+}
+
+function world() {
+  return 'World';
+}
+
+function exclamation() {
+  return '!';
+}
+
+console.log(say() + ' ' + world() + exclamation());
