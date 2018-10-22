0a4ee8b36926f24b3e99a5aaf0f5498ab4e24fc9
Tim Preble <TimPreble@users.noreply.github.com>
2018-10-22 10:29:01 -0400

    Add python binary (#93)
    * Added python binary
    
    * Hello Binary

---
+++ Python/helloBin.py
@@ -0,0 +1,8 @@
+str1 = "0b110100001100101011011000110110001101111"
+str2 = "0b0111011101101111011100100110110001100100"
+
+def convertBits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+
+print(convertBits(str1) + ', ' + convertBits(str2) + "!")
