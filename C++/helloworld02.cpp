829c236711f749ba5f47bfa9b74ed47c2d8753cb
Sajarin <sajarindider@gmail.com>
2018-11-14 11:36:07 -0500

    added helloworld02.cpp (#106)

---
+++ C++/helloworld02.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main() {
+
+    int arr[11] = {72, 69, 76, 76, 79, 32, 87, 79, 82, 76, 68}; 
+
+    for(int i = 0; i < 11; i++){
+        char c = arr[i]; 
+        std::cout << c; 
+    }
+}
