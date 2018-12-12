5c9d533935bba77cd072bc99dca44cdc3cb8311e
Saksham <sakshamr@protonmail.com>
2018-12-13 00:41:49 +0530

    Weird c++ Code

---
+++ C++/helloworldweird.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string> 
+using namespace std;
+
+#define PI "H";
+#define PIPI "e";
+#define PIPIPI "l";
+#define PIPIPIPI "o";
+#define PIPIPIPIPI "W";
+#define PIPIPIPIPIPI "r";
+#define PIPIPIPIPIPIPI "d";
+#define PIPIPIPIPIPIPIPI " ";
+#define PIPIPIPIPIPIPIPIPI "!";
+
+int main() {
+    string s = PI;
+    s += PIPI;
+    s += PIPIPI;
+    s += PIPIPI;
+    s += PIPIPIPI;
+    s += PIPIPIPIPIPIPIPI;
+    s += PIPIPIPIPI;
+    s += PIPIPIPI;
+    s += PIPIPIPIPIPI;
+    s += PIPIPI;
+    s += PIPIPIPIPIPIPI;
+    s += PIPIPIPIPIPIPIPIPI;
+    cout<<s;
+    return 0;
+}
