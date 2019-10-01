3edc6c9d85f84329c44de1aa58ae57d7a761f9da
Samansh Khandelwal <33225901+samanshk@users.noreply.github.com>
2019-10-01 16:02:21 +0530

    Hello world program using nested if statements

---
+++ Javascript/if_hello.js
@@ -0,0 +1,24 @@
+let hello = 'Hello World';
+
+if (hello[0] === 'H') {
+    if (hello[1] === 'e') {
+        if (hello[2] === 'l') {
+            if (hello[3] === 'l') {
+                if (hello[4] === 'o') {
+                    if (hello[5] === ' ') {
+                        if (hello[6] === 'W') {
+                            if (hello[7] === 'o') {
+                                if (hello[8] === 'r') {
+                                    if (hello[9] === 'l') {
+                                        if (hello[10] === 'd') 
+                                            console.log(hello);                                                             
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }    
+}
