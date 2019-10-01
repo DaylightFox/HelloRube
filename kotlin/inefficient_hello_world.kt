c499f06c0768c5d754234778d239f9b1edd30130
Maxime Lumeau <maxime.lumeau@gmail.com>
2019-10-01 16:44:52 +0200

    Create inefficient_hello_world.kt

---
+++ kotlin/inefficient_hello_world.kt
@@ -0,0 +1,56 @@
+//Inefficient Hello World in Kotlin
+
+fun main(args: Array<String>) {
+    h()
+    e()
+    l()
+    l()
+    o()
+    comma()
+    space()
+    w()
+    o()
+    r()
+    l()
+    d()
+}
+
+fun h() {
+    print("h")
+}
+
+fun e() {
+    print("e")
+}
+
+fun l() {
+    print("l")
+}
+
+fun o() {
+    print("o")
+}
+
+fun comma() {
+    print(",")
+}
+
+fun space() {
+    print(" ")
+}
+
+fun w() {
+    print("w")
+}
+
+fun r() {
+    print("r")
+}
+
+fun l() {
+    print("l")
+}
+
+fun d() {
+    print("d")
+}
