e376aac3e5db1b2db1d5b35a378b7d4e2cd5bfd4
Nicola Iacovelli <nicola.iacovelli@soft.it>
2019-10-04 11:28:38 +0200

    Added 8-digits stye hello world

---
+++ kotlin/eight_digit_hello_world.kt
@@ -0,0 +1,14 @@
+
+val firstLine : String =  """     _        _              _   _      _      """
+val secondLine : String = """|_| |_ |  |  | |    \    /  | | |_| |  | \  | |"""
+val thirdLine : String =  """| | |_ |_ |_ |_|     \/\/   |_| | \ |_ |_/  o o"""
+
+fun main(args: Array<String>) {
+    printHelloWorld()
+}
+
+fun printHelloWorld() {
+    println(firstLine)
+    println(secondLine)
+    println(thirdLine)
+}
