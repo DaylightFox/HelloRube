f1e4d5eb2e1e313cc7953c2904dbdb479e847023
root <root@localhost.localdomain>
2019-10-11 20:18:31 +0530

    Hello World Program using Concatenation

---
+++ Python/concatenation_Hello_world.py
@@ -0,0 +1,14 @@
+# Hello World Program Through Concatenation of String's
+
+HF1 = 'H'
+HF2 = 'e'
+HF3 = 'l'
+HF4 = 'l'
+HF5 = 'o'
+HF6 = 'W'
+HF7 = 'o'
+HF8 = 'r'
+HF9 = 'l'
+HF10 ='d'
+
+print(HF1+HF2+HF3+HF4+HF5+" "+HF6+HF7+HF8+HF9+HF10)
